683f62bfdd62606c8062d73583cb03c428344eb5
robmckay <robmckay@users.noreply.github.com>
2017-02-17 14:57:07 +0000

    Add files via upload

--- final.ino
+++ final.ino
@@ -1,12 +1,21 @@
 // FASTLED stuff
-#include "FastLED.h"
-#define NUM_LEDS 258
-#define DATA_PIN 1
+#include <FastLED.h>
+#define NUM_LEDS 256
+
 CRGB leds[NUM_LEDS];
 
 //MSGEQ7 stuff
 #include "MSGEQ7.h"
 #define pinAnalogLeft A0
+
+//PCB
+//#define DATA_PIN 4
+//#define pinReset 14
+//#define pinStrobe 16
+
+
+// My Room
+#define DATA_PIN 5
 #define pinReset 4
 #define pinStrobe 2
 #define MSGEQ7_INTERVAL ReadsPerSecond(50)
@@ -15,40 +24,62 @@ CMSGEQ7<MSGEQ7_SMOOTH, pinReset, pinStrobe, pinAnalogLeft> MSGEQ7;
 
 //WIFI Stuff
 #include <ESP8266WiFi.h>
+//const char* ssid = "124WR-Kitchen";
+//const char* password = "JonasCourtH3";
 const char* ssid = "BTHub5-NQ38";
 const char* password = "schoolroadmassive";
 WiFiServer server(80);
 
-int bass, mid, treble, inputBass, lastBass, deltaBass, lastMid, deltaMid;
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> 
+
+
+int bass, mid, treble, inputBass, lastBass, deltaBass, lastMid, deltaMid,volume,deltaVolume, lastVolume;
 bool onOff = true;
 bool brightnessChange = false;
 int brightness = 200;
-int current = 5;
+int current = 9;
 bool wifiOn = true;
 int simpleColour = 0;
 int orange = 0;
 bool meggyness = false;
 int colour = 160;
+int variable = 10;
+int sensitivity = 20;
+bool strobeNow = false;
+int strobeLast = 0;
+
 
 void setup() {
   Serial.begin(115200);
   // This will set the IC ready for reading
+    WiFi.hostname("robslights");
+    //WiFi.hostname("ed");
+     WiFiManager wifiManager;
+     
+      //wifiManager.resetSettings();
+      //wifiManager.setSTAStaticIPConfig(IPAddress(192,168,123,123), IPAddress(192,168,0,1), IPAddress(255,255,255,0));
+      wifiManager.autoConnect("LEDSetup");
+
+ 
   MSGEQ7.begin();
   
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
-  delay(10);
-  if(wifiOn){
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("connecting... ");
-  }
-  Serial.println("WiFi connected");
-  Serial.println(WiFi.localIP());
-  server.begin();
-  }
+ // delay(10);
+//  if(wifiOn){
+//    // Connect to WiFi network
+//    WiFi.begin(ssid, password);
+//    while (WiFi.status() != WL_CONNECTED) {
+//      delay(500);
+//      Serial.println("connecting... ");
+//    }
+//    Serial.println("WiFi connected");
+//    Serial.println(WiFi.localIP());
+//    
+server.begin();
+//  }
   
   
   pinMode(15,INPUT);
@@ -58,66 +89,133 @@ void loop() {
    if(wifiOn){
     WiFiClient client = server.available();
     if(client){
-      // Read the first line of the request
-      String request = client.readStringUntil('\r');
-      Serial.println(request);
-      client.flush();
-      String s = "HTTP/1.1 200 OK\r\n";
-      s += "Content-Type: text/html\r\n\r\n";
-      s += "<!DOCTYPE HTML>\r\n<html></html>\r\n";
-      client.println(s);
-    
-      if(request.indexOf("white") != -1){
-        current = 0;
-       
-      }
-      if(request.indexOf("onOff") != -1){
-        onOff = !onOff;
-       
-      }
-      if(request.indexOf("brightnessUp") != -1){
-        brightness = brightness +20;
-        if(brightness >255){
-          brightness = 255;
-        }
-       
-      }
-      if(request.indexOf("brightnessDown") != -1){
-        if(brightness <19){
-          brightness = 15;
-        }else{
-          brightness = brightness -20;
-        }
-       
-      }
-      if(request.indexOf("changeProgram") != -1){
-        current++;
-        if(current > 5  ){
-          current = 1;
-        }
-       
-      }
-      if(request.indexOf("orange") != -1){
-        if(orange < 5){
-        orange++;
-        }else{
-          orange = 0;
-        }
-       
-      }
-      if(request.indexOf("meg") != -1){
-        meggyness=!meggyness;
-       
-      }
+      if(client.available()){
+        // Read the first line of the request
+        String request = client.readStringUntil('\r');
+        Serial.println(request);
+        client.flush();
+        String s = "HTTP/1.1 200 OK\r\n";
+        s += "Content-Type: text/html\r\n\r\n";
+        s += "<!DOCTYPE HTML>\r\n<html>";
+        s += "<style>button{font-size: 100px;}</style>";
+        s += "<h1>Party Selecter</h1>";
+        s += "<br><a href=\"strobe\"><button>STROBE</button></a></br>";
+        s += "<br><a href=\"onOff\"><button>On Off</button></a></br>";
+        s += "<br><a href=\"upSensitivity\"><button>+ sensitivity</button></a></br>";
+        s += "<br><a href=\"downSensitivity\"><button>- sensitivity</button></a></br>";
+        s += "<br><a href=\"white\"><button>White</button></a></br>";
+   
+        s += "<br><a href=\"fillColour\"><button>Rainbow to the beat</button></a></br>";
+        s += "<br><a href=\"beatSploge\"><button>Beat Sploges</button></a></br>";
+        s += "<br><a href=\"strobeSetTime\"><button>Nice and Strobey</button></a></br>";
+        s += "<br><a href=\"changeProgram\"><button>Cycle party lighting</button></a></br>";
+        s += "<br><a href=\"bassMidBars\"><button>RGB to the beat</button></a></br>";
+        s += "<br><a href=\"simpleLightBeat\"><button>Strip cycle rainbow to beat</button></a></br>";
+        s += "<br><a href=\"bassBullets\"><button>Bass bullets</button></a></br>";
+        s += "<br><a href=\"strobeBass\"><button>Maybe too strobey?</button></a></br>";
+        s += "<br><a href=\"colourMove\"><button>Party Rainbow to the beat</button></a></br>";
+        
+        s += "</html>\r\n";
+        client.println(s);
       
+        if(request.indexOf("white") != -1){
+          current = 0;
+         
+        }
+        if(request.indexOf("onOff") != -1){
+          onOff = !onOff;
+         
+        }
+        if(request.indexOf("brightnessUp") != -1){
+          brightness = brightness +20;
+          if(brightness >255){
+            brightness = 255;
+          }
+         
+        }
+        if(request.indexOf("brightnessDown") != -1){
+          if(brightness <19){
+            brightness = 15;
+          }else{
+            brightness = brightness -20;
+          }
+         
+        }
+        if(request.indexOf("changeProgram") != -1){
+          current++;
+          if(current > 8 ){
+            current = 1;
+          }
+          sensitivity = 20;
+         
+        }
+        if(request.indexOf("orange") != -1){
+          if(orange < 5){
+          orange++;
+          }else{
+            orange = 0;
+          }
+         
+        }
+        if(request.indexOf("meg") != -1){
+          meggyness=!meggyness;
+         
+        }
+        if(request.indexOf("fillColour") != -1){
+          current = 1;  
+          sensitivity = 20;  
+        }
+        if(request.indexOf("bassMidBars") != -1){
+          current = 2;
+          sensitivity = 20;
+        }
+        if(request.indexOf("simpleLightBeat") != -1){
+          current = 3;
+          sensitivity = 20;
+        }
+        if(request.indexOf("bassBullets") != -1){
+          current = 4;
+          sensitivity = 20;
+        }
+        if(request.indexOf("strobeBass") != -1){
+          current = 5;
+          sensitivity = 20;
+        }
+        if(request.indexOf("colourMove") != -1){
+          current = 6;
+          sensitivity = 20;
+        }
+        if(request.indexOf("strobeSetTime") != -1){
+          current = 7;
+          sensitivity = 20;
+        }
+        if(request.indexOf("beatSploge") != -1){
+          current = 8;
+          sensitivity = 20;
+        }
+  
+        if(request.indexOf("upSensitivity") != -1){
+          sensitivity += 3;
+        }
+        if(request.indexOf("downSensitivity") != -1){
+          sensitivity -= 3;
+        }
+        if(request.indexOf("strobe") != -1){
+          strobeNow = true;
+        }
+      }
       client.stop();
     }
    }
 
   
   if(digitalRead(15) == HIGH){
-    onOff = !onOff;
-    delay(500);
+    current++;
+    if(current > 6  ){
+      current = 1;
+    }
+    sensitivity = 20;
+    delay(300);
   }
   if(onOff){
     bool newReading = MSGEQ7.read(MSGEQ7_INTERVAL);
@@ -129,13 +227,13 @@ void loop() {
         // Analyze without delay every interval
         
         setupValues();
-        nicksIdea();
-        delay(2);
+        fillColour();
+        delay(15);
         break;
       case 2:
-        
+        setupValues();
         bassMidBars();
-        delay(3);
+        delay(6);
         break;
       case 3:
         setupValues();
@@ -149,13 +247,230 @@ void loop() {
         setupValues();
         strobeBass();
         break;
+      case 6:
+        setupValues();
+        colourMove();
+        break;
+      case 7:
+        setupValues();
+        strobeSetTime();
+        break;
+      case 8:
+        setupValues();
+        setBeats();
+        break;
+      case 9:
+        setupValues();
+        reggaeMiddle();
+        break;
     }
+    
     FastLED.setBrightness(brightness);
+    if(strobeNow){
+      if((millis() - strobeLast) >10){
+        fill_solid( leds, NUM_LEDS, CRGB(255, 255, 255)); 
+        strobeLast = millis();
+      }
+      strobeNow = false;
+    }
     FastLED.show();
   }else{
     fill_solid( leds, NUM_LEDS, CRGB(0, 0, 0)); 
     FastLED.show();
   }
+
+  if (Serial.available() > 0) {
+    variable = Serial.parseInt(); 
+  }
+}
+int strobeCount = 0;
+int lastStrobe = 0;
+int movementStrobe = 0;
+
+void reggaeMiddle(){
+  int middle = NUM_LEDS/2;
+
+  int bassMap = map(bass,100,255,middle,NUM_LEDS);
+  fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
+  for(int i =middle; i<= bassMap; i++){
+    leds[i] = CHSV(colour,255,255);
+    
+  }
+  bassMap = map(bass,100,255,middle,0);
+  for(int i =middle; i>= bassMap; i--){
+    leds[i] = CHSV(colour,255,255);
+    
+  }
+  colour++;
+  
+}
+
+
+
+
+
+int movement = 0;
+void setBeats(){
+  deltaBass = bass - lastBass;
+  lastBass = bass;
+  deltaMid = mid - lastMid;
+  lastMid = mid;
+  
+  deltaVolume = volume - lastVolume;
+  lastVolume = volume;
+  
+//  if(deltaBass >10  ){
+//    movement += 40;
+//  }
+//  if(millis()%10){
+//  for(int i = 0; i<= NUM_LEDS;i++){
+//    volume = map(volume, 0,255,10,40);
+//    if(!leds[i]){
+//    leds[i] += ColorFromPalette( PartyColors_p , (i+movement)/2,volume);
+//    }
+//  }
+//  }
+  
+  if(deltaBass > sensitivity-7){
+    int randomMid = random(30,NUM_LEDS -30);
+    int counter = 0;
+    for(int i = randomMid -30; i<= randomMid +30; i++){
+      if(counter <=30){
+        int dither = map(counter, 0,30,0,255);
+        leds[i] = CHSV(colour-15,255,dither);
+      }else{
+        int dither = map(counter, 30,60,255,0);
+        leds[i] = CHSV(colour-15,255,dither);
+      }
+      counter++;
+    }
+    colour += 2;
+  }
+
+
+
+  if(deltaMid > sensitivity-8){
+    int randomMid = random(30,NUM_LEDS -30);
+    int counter = 0;
+    for(int i = randomMid -30; i<= randomMid +30; i++){
+      if(counter <=30){
+        int dither = map(counter, 0,30,0,255);
+        leds[i] = CHSV(colour,255,dither);
+      }else{
+        int dither = map(counter, 30,60,255,0);
+        leds[i] = CHSV(colour,255,dither);
+      }
+      counter++;
+    }
+    colour += 2;
+  }
+  if(deltaVolume > sensitivity-7){
+    if(millis()%6){
+      int randomMid = random(30,NUM_LEDS -30);
+      int counter = 0;
+      for(int i = randomMid -30; i<= randomMid +30; i++){
+        if(counter <=30){
+          int dither = map(counter, 0,30,0,255);
+          leds[i] = CHSV(colour+15,255,dither);
+        }else{
+          int dither = map(counter, 30,60,255,0);
+          leds[i] = CHSV(colour+15,255,dither);
+        }
+        counter++;
+      }
+      colour += 2;
+    }
+  }
+  
+
+  if(millis()%2 ){
+    for(int i; i<= NUM_LEDS;i++){
+      leds[i] %= 230;
+    }
+  }
+
+
+}
+
+
+void strobeSetTime(){
+//  if(millis() %3 ==0){
+//     movementStrobe += 2;
+//  }
+//  if(millis() %5 ==0){
+//     deltaMid = mid - lastMid;
+//    lastMid = mid;
+//    mid = mid- (deltaMid/3);
+//    for(int i = 0; i<= NUM_LEDS;i++){
+//      
+//      leds[i] = ColorFromPalette( PartyColors_p, (i+movementStrobe)/3);
+//    }
+//    for(int i = 0; i<= NUM_LEDS;i++){
+//      
+//      leds[i] -= CRGB(210 - mid,140 - mid,210 - mid);
+//    }
+//  }
+   int mapBass = map(bass, 0,255, 0,10);
+    movementStrobe += mapBass + (sensitivity -20);
+    for(int i = 0; i<= NUM_LEDS;i++){
+      
+      leds[i] = ColorFromPalette( PartyColors_p, (i+movementStrobe)/3);
+      leds[i] -= CRGB(100,40,100);
+    }
+  
+  deltaBass = bass - lastBass;
+  lastBass = bass;
+  if(deltaBass > (sensitivity +15) ){
+    if((millis() - lastStrobe) > 300){
+      lastStrobe = millis();
+      strobeCount = 0;
+    }
+  }
+  if(strobeCount <= 6){
+    if(strobeCount %2 == 0){
+      delay(20);
+      fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
+    }else{
+      delay(20);
+      fill_solid(leds, NUM_LEDS, CRGB(255,255,255));
+    }
+    strobeCount++;
+  }else{
+    
+  }
+}
+
+
+
+
+
+void fillColour(){
+  if(millis() %3 ==0){
+     movement += 4;
+  }
+ 
+  for(int i = 0; i<= NUM_LEDS;i++){
+    
+    leds[i] = ColorFromPalette( PartyColors_p, (i+movement)/3);
+  }
+  for(int i = 0; i<= NUM_LEDS;i++){
+    
+    leds[i] -= CRGB(210 - bass,210 - bass,210 - bass);
+  }
+}
+
+
+
+void colourMove(){
+   deltaBass = bass - lastBass;
+  lastBass = bass;
+  int mapBass = map(bass, 0,255, 0,10);
+  movement += mapBass + (sensitivity -20);
+  for(int i = 0; i<= NUM_LEDS;i++){
+    
+    leds[i] = ColorFromPalette( PartyColors_p, (i+movement)/3);
+  }
+  
 }
 int strobeCounter =0;
 bool bassStrobeOn = false;
@@ -170,12 +485,10 @@ void strobeBass() {
   if(millis() %100 ==0){
     colour+= 4;
   }
-  if(deltaBass > 18){
-    bassStrobeOn = true;
-  }
-  if(bassStrobeOn && bass >70){
+  if(deltaBass > (sensitivity +5) ){
+    
     colour += 3;
-    fill_solid(leds, NUM_LEDS, CHSV(bass,255,255));
+    fill_solid(leds, NUM_LEDS, CHSV(bass,variable,255));
      
      FastLED.show();
      delay(15);
@@ -183,8 +496,6 @@ void strobeBass() {
      FastLED.show();
      delay(15);
     strobeCounter= strobeCounter +5;
-  }else{
-    bassStrobeOn = false;
   }
    
    
@@ -198,12 +509,20 @@ int bassBulletArray[50];
 int bassBulletCounter = 0;
 int midBulletArray[50];
 int midBulletCounter = 0;
-
 bool allBlack = true;
-
+//CRGBPalette16 currentPalette = (
+//                                   0x9700FF,  0x310CE8,  0x003FFF,  0x0C9CE8,
+//                                     0x05FFED );
 void bassBullets(){
   fadeToBlackBy(leds,NUM_LEDS,100);
-
+  if(deltaBass >10  ){
+    movement += 40;
+  }
+  for(int i = 0; i<= NUM_LEDS;i++){
+    volume = map(volume, 0,255,10,40);
+    leds[i] = ColorFromPalette( PartyColors_p , (i+movement)/2,volume);
+  }
+  
   //For the bass
   for(int i = 0; i <49; i++){
       if(bassBulletArray[i] <(NUM_LEDS-1)){
@@ -212,7 +531,7 @@ void bassBullets(){
   }
   deltaBass = bass - lastBass;
   lastBass = bass;
-  if(deltaBass >20){
+  if(deltaBass >30){
     bassBulletArray[bassBulletCounter] = 0;
     bassBulletCounter++;
     colour++;
@@ -235,7 +554,7 @@ void bassBullets(){
   }
   deltaMid = mid - lastMid;
   lastMid = mid;
-  if(deltaMid >25){
+  if(deltaMid >28){
     midBulletArray[midBulletCounter] = 0;
     midBulletCounter++;
   }
@@ -250,8 +569,9 @@ void bassBullets(){
 
   
   
-  if(deltaBass >35){
+  if(deltaBass > (sensitivity + 13)){
     fill_solid(leds,NUM_LEDS,CHSV(255,0,255));
+    delay(2);
   }
 
   for(int i = 0; i <= NUM_LEDS; i++){
@@ -319,6 +639,8 @@ void setupValues(){
   bass = mapNoise(bass);
   mid = mapNoise(mid);
   treble = mapNoise(treble);
+  volume = MSGEQ7.getVolume();
+  volume = mapNoise(volume);
 }
 
 
@@ -366,60 +688,17 @@ void midBeat(){
 
 //where bass and mid are seperate and each varys with bars of different length on beat
 void bassMidBars() {
-  int baseline = 50;
-  
-  // Visualize the average bass of both channels
-  uint8_t inputBass = MSGEQ7.get(MSGEQ7_BASS);
-  inputBass = mapNoise(inputBass);
-  static uint8_t lastInputBass = 0;
-  int deltaBass = inputBass - lastInputBass;
-  lastInputBass = inputBass;
-
-//  if (deltaBass >= 20){
-//    fill_solid( leds, NUM_LEDS, CRGB(inputBass,000,000)); 
-//    FastLED.show();
-//    Serial.println("bass");
-//    
-//  }
-  
-  if(inputBass < baseline-20){
-    inputBass =0;
-  }else{
-    inputBass = inputBass -30;
-  }
-    uint8_t inputMid = MSGEQ7.get(MSGEQ7_MID);
-  inputMid = mapNoise(inputMid);
-  if(inputMid < (baseline )){
-    inputMid =0;
-  }else{
-    inputMid = inputMid -50;
-  }
-    uint8_t inputHigh = MSGEQ7.get(MSGEQ7_HIGH);
-  inputHigh = mapNoise(inputHigh);
-  if(inputHigh < (baseline + 20)){
-    inputHigh =0;
-  }else{
-    inputHigh = inputHigh -70;
-  }
-   int bassLevel = map(inputBass, 0, 255, 0, 117);
-   for (int i=0; i <= bassLevel; i++){
-      leds[i] = CRGB(255,0,0);
-   }
-   for (int i=bassLevel; i <= 117; i++){
-      leds[i] = CRGB(100,100,100);
-   }
-   int midLevel = map(inputMid, 0, 255, 117, 183);
-   for (int i=117; i <= midLevel; i++){
-      leds[i] = CRGB(0,255,0);
-   }
-   for (int i=midLevel; i <= 183; i++){
-      leds[i] = CRGB(100,100,100);
-   }
-   
-    for (int i=183; i <= 258; i++){
-      leds[i] = CRGB(0,0,inputHigh);
-      
-   } 
+  fill_solid( leds, NUM_LEDS, CRGB(0, 0, 0));
+ for(int i = 0; i <= (NUM_LEDS/3); i++){
+   leds[i] = CRGB(bass,0,0);
+ }
+ for(int i = (NUM_LEDS/3); i <= ((NUM_LEDS/3)*2); i++){
+   leds[i] = CRGB(0,mid,0);
+ }
+ for(int i = ((NUM_LEDS/3)*2); i <= NUM_LEDS; i++){
+   leds[i] = CRGB(0,0,treble);
+ }
+ 
 
 }
 
